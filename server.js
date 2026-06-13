67ca971d62b76253d892abe985c43c7a369d6d43
JustMistDragoon <mistdragoon2963@gmail.com>
2026-06-13 18:56:24 +0400

    fixed

--- server.js
+++ server.js
@@ -7,19 +7,17 @@ app.use(cors());
 app.use(express.json());
 
 // Mount API routes
+import healthHandler from "./api/health.js";
 import signupHandler from "./api/auth/signup.js";
 import signinHandler from "./api/auth/signin.js";
 import bookHandler from "./api/book.js";
 
+app.get("/api/health", (req, res) => healthHandler(req, res));
 app.post("/api/auth/signup", (req, res) => signupHandler(req, res));
 app.post("/api/auth/signin", (req, res) => signinHandler(req, res));
 app.all("/api/book", (req, res) => bookHandler(req, res));
 
 const PORT = process.env.PORT || 3001;
-if (process.env.NODE_ENV !== "vercel") {
-  app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-  });
-}
-
-export default app;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
